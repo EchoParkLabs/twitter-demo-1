c5eee5d0cad1fa34d7cd3fdc356c81078aef8a8c
David Raleigh <davidraleigh@gmail.com>
2017-11-06 20:35:28 -0800

    trying TwitterAPI

--- image_post.py
+++ image_post.py
@@ -1,2 +1,16 @@
-from twitter.api import Twitter
+import os
+from TwitterAPI import TwitterAPI
+from epl.imagery.reader import Landsat, Metadata
 
+cons_key = os.environ['CONSUMER_KEY_API']
+cons_secret = os.environ['CONSUMER_SECRET_API']
+access_token = os.environ['ACCESS_TOKEN']
+access_secret = os.environ['ACCESS_TOKEN_SECRET']
+
+api = TwitterAPI(cons_key, cons_secret, access_token, access_secret)
+
+r = api.request('statuses/update', {'status':'This is a tweet!'})
+print(r.status_code)
+# png = "./trace1.png"
+
+print("hello twitter")

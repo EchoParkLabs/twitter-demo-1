29c9deadd2303b86ee0383cc79dad1323f540514
David Raleigh <davidraleigh@gmail.com>
2017-11-13 14:52:30 -0800

    remove overview images

--- image_post.py
+++ image_post.py
@@ -280,8 +280,8 @@ def main(argv):
                 if image_extent.contains(STATE_COUNTY_MAP[county]):
                     contained_counties.append(county)
 
-            post_image(metadata, date_string, api)
-            tweet_count += 1
+            # post_image(metadata, date_string, api)
+            # tweet_count += 1
             for county_name in contained_counties:
                 post_image(metadata, date_string, api, STATE_COUNTY_MAP[county_name])
                 tweet_count += 1
